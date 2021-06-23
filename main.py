a6a31e5ef4b77f80fafb57aa400578239ceafe3d
Stacey31 <53440572+Stacey-31@users.noreply.github.com>
2021-06-23 22:43:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#Step1
+name = "Lebron" #string
+age = 35 #integer
+salary = 37.44  #float
+plays_basketball = True #boolean
+jersey_number = "23" #string
+
+
+#Step 2
+car_name = "Mercedes"
+x = 50
+
+my_first_name = "Stacey"
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
+print(type(x))
+print(type(my_first_name))
